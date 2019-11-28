0dc356aee195b4e2b3367f9b43e88a02a919ff12
JakeFromSnakeFarm <jammons1@gmail.com>
2019-11-28 17:45:49 -0500

    Added Styled Checkbox
    Styled checkboxes to match current theme

--- src/components/BoolSettingInput.vue
+++ src/components/BoolSettingInput.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="half_select_setting">
 		<span class="select_label">{{ title }}</span>
-
-		<input type="checkbox" v-model="value">
+		<label class="check_container">
+			<input type="checkbox" v-model="value">
+			<span class="check"></span>
+		</label>
 	</div>
 </template>
 
@@ -37,5 +39,46 @@ export default {
 	height: 6vh;
 	width: 90%;
 }
-
+.check_container {
+	display: block;
+	position: relative;
+	cursor: pointer;
+	user-select: none; 
+}
+.check_container input {
+	position: absolute;
+	opacity: 0;
+	height: 0;
+	width: 0;
+	cursor: pointer;
+}
+.check {
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 25px;
+	width: 25px;
+	background-color: #FFFFFF;
+}
+.check_container:hover input ~ .check {
+	 background-color: #ccc;
+}
+.check:after {
+	content: "";
+	position: absolute;
+	display: none;
+	left: 9px;
+	top: 5px;
+	width: 5px;
+	height: 10px;
+	border: solid white;
+	border-width: 0 3px 3px 0;
+	transform: rotate(45deg);
+}
+.check_container input:checked ~ .check:after {
+	display: block;
+}
+.check_container input:checked ~ .check {
+	background-color: #3498db;
+}
 </style>
