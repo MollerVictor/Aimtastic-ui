c77c9afa3d195dd6831946adb2d967e0b5c3d96f
JakeFromSnakeFarm <jammons1@gmail.com>
2019-12-19 21:16:17 -0500

    Settings Screen height changes

--- src/components/Settings/ColorSettingInput.vue
+++ src/components/Settings/ColorSettingInput.vue
@@ -157,7 +157,6 @@ export default {
 	padding-left: 0.6vh;
 	padding-left: 1.25vh;
 	display: flex;
-	height: 6vh;
 	width: 90%;
 }
 
--- src/components/Settings/IntSettingInput.vue
+++ src/components/Settings/IntSettingInput.vue
@@ -53,7 +53,6 @@ export default {
 	padding-left: 0.6vh;
 	padding-left: 1.25vh;
 	display: flex;
-	height: 6vh;
 	width: 90%;
 }
 
