777eb4cf38223ca9bb3dc1cde8654901654963a3
Victor Möller <victor@victormoller.com>
2020-01-05 18:07:30 +0100

    Added dlc overlaay and ask to buy dlc button

--- src/components/GameModeButton.vue
+++ src/components/GameModeButton.vue
@@ -1,7 +1,10 @@
 <template>
-	<div onmouseover="ENGINE_playHoverSound()" onclick="ENGINE_playClickSound(); showRoomSettings(this);" class="play_gamemode button-sound get-closer" :data-room="roomName">
+	<div onmouseover="ENGINE_playHoverSound()" onclick="ENGINE_playClickSound(); showRoomSettings(this);" class="play_gamemode button-sound get-closer" :data-isDLC="isDLC" :data-room="roomName">
 		<img class="background-image"  :src="require(`../assets/images/rooms/${imageUrl}.png`)" rel="preload"/>
 		<div class="box-title">{{ title }}</div>
+		<div v-if="isDLC == true && hasDLC.value == false" class="dlc_overlay">
+			<div class="dlc_bottom_text">DLC</div> 
+		</div>
 	</div>
 </template>
 
@@ -11,14 +14,47 @@
 //This is a way to get around that
 window.showRoomSettings = identifier => 
 {
-	var roomName = identifier.getAttribute("data-room");
-	ENGINE_requstRoomSettings(roomName);
+	var dlc = identifier.getAttribute("data-isDLC");
+	if(dlc && !window.hasDLC.value){
+		window.showDLCPopup.value = true;
+	}
+	else{
+		var roomName = identifier.getAttribute("data-room");
+		ENGINE_requstRoomSettings(roomName);
+	}
 }
 
 
 /* global ENGINE_requstRoomSettings */
 export default {
 	name: "GameModeButton",
-	props: ["title", "roomName", "imageUrl"],
+	props: ["title", "roomName", "imageUrl", "isDLC"],
+
+	data() {
+		return {
+			hasDLC: window.hasDLC,
+
+		};
+	},
 };
 </script>
+
+<style scoped>
+.dlc_overlay{
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+
+	background-color: rgba(128, 128, 128, 0.336);
+}
+
+.dlc_bottom_text{
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	font-size: 3vh;
+}
+
+</style>
--- src/components/PlayScreen.vue
+++ src/components/PlayScreen.vue
@@ -5,7 +5,7 @@
 				<div class="gamemode-column">		
 					<GameModeButton title="Flat Wall Tracking" roomName="TrackingRoom" imageUrl="traking"/>
 					<GameModeButton title="360 Tracking" roomName="360TrackingRoom" imageUrl="360"/>
-					<GameModeButton title="(DLC) Snaking" roomName="SnakeRoom" imageUrl="360"/>
+					<GameModeButton title="(DLC) Snaking" roomName="SnakeRoom" imageUrl="360" :isDLC="true"/>
 					<GameModeButton title="Simple Circle" roomName="SimpleCircleRoom" imageUrl="simple"/>
 					<GameModeButton title="Reflex" roomName="ReflexRoom" imageUrl="reflex"/>
 					<GameModeButton title="Left/Right Flicks" roomName="LeftRightRoom" imageUrl="left"/>
@@ -15,24 +15,25 @@
 			</div>  
 			<div><h1>Flicks</h1>
 				<div class="gamemode-column">
-					<GameModeButton title="(DLC) Sphere Flicks (Moving)" roomName="TrackingOnMovingTargets" imageUrl="360"/>
-					<GameModeButton title="(DLC) Sphere Flicks (Static)" roomName="TrackingOnStaticTargets" imageUrl="360"/>
+					<GameModeButton title="(DLC) Sphere Flicks (Moving)" roomName="TrackingOnMovingTargets" :isDLC="true" imageUrl="360"/>
+					<GameModeButton title="(DLC) Sphere Flicks (Static)" roomName="TrackingOnStaticTargets" :isDLC="true" imageUrl="360"/>
 					<GameModeButton title="Bigger Then Smaller" roomName="BiggerThenSmallerRoom" imageUrl="bigger"/>
 					<GameModeButton title="Popup Targets Flicks" roomName="PopupTargetsRoom" imageUrl="pupoo"/>		
-					<GameModeButton title="(DLC) Flicks Flicks Flicks" roomName="FlicksFlicksFlicksRoom" imageUrl="360"/>
-					<GameModeButton title="(DLC) 180 Flicks" roomName="180FlicksRoom" imageUrl="360"/>
-					<GameModeButton title="(DLC) Tile Frenzy" roomName="TileFrenzy" imageUrl="360"/>
+					<GameModeButton title="(DLC) Flicks Flicks Flicks" roomName="FlicksFlicksFlicksRoom" 	:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="(DLC) 180 Flicks" roomName="180FlicksRoom" 						:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="(DLC) Tile Frenzy" roomName="TileFrenzy" 						:isDLC="true" imageUrl="360"/>
 				</div>
 			</div>
 			<div><h1>Assault Maps</h1>
 				<div class="gamemode-column">
 					<GameModeButton title="Assault Course 1" roomName="Assult Course Uno" imageUrl="360"/>
-					<GameModeButton title="Assault Course 2" roomName="Assault Course 2" imageUrl="360"/>
+					<GameModeButton title="Assault Course 2" roomName="Assault Course 2" 				:isDLC="true" imageUrl="360"/>
 					<GameModeButton title="Reaction Time" roomName="ReactionsRoom" imageUrl="reaction"/>
-					<GameModeButton title="(DLC) Training Zone 1" roomName="Training Zone 1" imageUrl="360"/>
-					<GameModeButton title="(DLC) Forever Attacking Droids" roomName="WarmupRoom" imageUrl="360"/>
-					<GameModeButton title="(DLC) Clicking Moving Targets" roomName="FlicksOnMovingTargets" imageUrl="360"/>
-					<GameModeButton title="(DLC) Clicking Static Targets" roomName="FlicksOnStaticTargets" imageUrl="360"/>
+					<GameModeButton title="Training Zone 1" roomName="Training Zone 1" 					:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="Forever Attacking Droids" roomName="WarmupRoom" 				:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="Forever Attacking Droids (University)" roomName="RA_Coast" 				:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="Clicking Moving Targets" roomName="FlicksOnMovingTargets" 	:isDLC="true" imageUrl="360"/>
+					<GameModeButton title="Clicking Static Targets" roomName="FlicksOnStaticTargets" 	:isDLC="true" imageUrl="360"/>
 				</div>
 			</div>
 		</div>
@@ -40,16 +41,66 @@
 		<div>
 			<button onmouseover="ENGINE_playHoverSound()" onclick="ENGINE_playClickSound()" class="ui button secondary">Close</button>
 		</div>
+
+		<diV class="dlc_popup" v-if="showDLCPopup.value">
+			<div class="dlc_popup_window">
+				This requires the DLC to play. Do you want to buy it now?
+
+				<div v-if="isOpeningSteamOverlay">
+					<p>Opening Steam overlay...<br/>
+						If it's not working please press <a style="color: blue;" @click="openDLCInBrowser">HERE</a> to open it in your browser.
+					</p>
+
+				</div>
+
+				<div class="button" @click="closePopup">Close</div>
+				<div class="button" @click="buyDLC">Buy</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+
+	/* global ENGINE_openDLCBuyPage ENGINE_openLink */
+
+	window.onHasFullGameDLC = function (jsonString) {
+		var parsedData = JSON.parse(jsonString);	
+
+		window.hasDLC.value = jsonString;
+	};
+
+	window.hasDLC = { value: false};
+	window.showDLCPopup = { value: false};
+
+
 	import GameModeButton from "./GameModeButton.vue";
 
 	export default {
 		name: "PlayScreen",
 		components: {
 			GameModeButton
+		},
+
+		
+		data() {
+			return {
+				hasDLC: window.hasDLC,
+				showDLCPopup: window.showDLCPopup,
+				isOpeningSteamOverlay: false,
+			};
+		},
+		methods:{
+			closePopup: function() {
+				this.showDLCPopup.value = false;
+			},
+			buyDLC: function(){
+				this.isOpeningSteamOverlay = true;
+				ENGINE_openDLCBuyPage();
+			},
+			openDLCInBrowser: function(){
+				ENGINE_openLink("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+			}
 		}
 	};
 </script>
@@ -72,4 +123,23 @@ h1 {
 	justify-content: space-between;
 	height: 100%;    
 }
+
+.dlc_popup{
+	position: absolute;
+	left: 0;
+	top:0;
+	width: 100%;
+	height: 100%;
+	background-color: #5252526e;
+	display: flex;
+	justify-content: center;
+	align-content: center;
+}
+
+.dlc_popup_window{
+	width: 50vh;
+	height: 30vh;
+	background-color: grey;
+}
+
 </style>
