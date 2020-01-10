82cf19a87bc80f468d6387bcb8f517ba87125923
Victor Möller <victor@victormoller.com>
2020-01-10 19:31:36 +0100

    Added workshop page

---
+++ src/components/WorkshopScreen.vue
@@ -0,0 +1,42 @@
+<template>
+	<diV  class="center_area fullscreen_window">
+		<button onclick="ENGINE_openLinkInSteam('https://steamcommunity.com/app/757240/workshop/')">Open Workshop Page</button>
+		
+		<div>
+			<div v-for="item of workshopItems.value" v-bind:key="item.Title">
+				{{item.Title}}
+			</div>
+		</div>
+
+	</div>
+</template>
+
+<script>
+
+	window.onWorkshopQueryEvent = function (jsonString) {
+		var parsedData = JSON.parse(jsonString);	
+
+		
+
+		window.workshopItems.value = parsedData;
+
+		console.log(parsedData);
+	};
+
+	window.workshopItems = { value: []};
+
+	export default {
+		name: "WorkshopScreen",
+
+		data() {
+			return {
+				workshopItems: window.workshopItems
+			};
+		},
+	}
+
+</script>
+<style scoped>
+
+
+</style>
